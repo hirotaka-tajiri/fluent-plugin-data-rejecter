7d2589a3809a7423c670ecc3ea13ddee2152b362
田尻 浩隆 <hiro@hiro-MacBook.local>
2014-01-11 02:38:52 +0900

    second commit

--- lib/fluent/plugin/out_data_rejecter.rb
+++ lib/fluent/plugin/out_data_rejecter.rb
@@ -8,27 +8,23 @@ module Fluent
 
         def configure(conf)
             super
-
             @remove = @remove_prefix.end_with?('.') ? @remove_prefix : @remove_prefix + '.'
             @add    = @add_prefix.end_with?('.')    ? @add_prefix    : @add_prefix    + '.'
             @add_p  = @add.chop
         end
 
         def emit(tag, es, chain)
-
             tmptag = (tag[@remove.size .. -1] if tag.start_with?(@remove)) || (tag == @remove_prefix ? '' : tag)
             tag    = @add_p.size.zero? ? (tmptag.size.zero? ? "data_rejecter.tag_lost" : tmptag) : (tmptag.size.zero? ? @add_p : @add + tmptag)
 
-            es.each {|time,record|
+            es.each {| time, record |
                 Engine.emit(tag, time, reject_record(record))
             }
-
             chain.next
         end
 
       private
         def reject_record(record)
-
             @reject_keys.split.each{| v | record.delete(v) if record.has_key?(v)}
             record
         end
